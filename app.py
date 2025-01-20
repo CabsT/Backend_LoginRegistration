d5b87577a21a8f5d38367c7cec30017b108f7347
CabsT <133862761+CabsT@users.noreply.github.com>
2025-01-20 13:04:31 +0200

    Add registration endpoint with CORS support and JSON logging
    - Created a POST `/register` route to handle user registration.
    - Added error handling to return a message if an error occurs during registration of post request
    - Integrated `flask-cors` for cross-origin resource sharing support.
    - Implemented logging for the incoming JSON data received in the registration request

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/register')
+@app.route('/register', methods= ['POST'])
 def register():
+    try:
+        # Debug log to check received data
+        print("Received JSON:", request.json)
+    except:
+        return({'message': 'An error occurred during registration'}),500
+    
+
     return 'User Registered'
 
 if __name__ == '__main__':
